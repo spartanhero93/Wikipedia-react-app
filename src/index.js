5f56c673b41b87b9bee7d5655144e313251dbba4
luwis lopez <spartanhero93@gmail.com>
2018-07-31 23:51:51 -0400

    initial build

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import configureStore from './store'
+import './index.css'
+import App from './Components/App'
+
+ReactDOM.render(
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
