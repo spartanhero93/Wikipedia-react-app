30d260025b54aa150e9008184944ab6e9a3e40e7
luwis lopez <spartanhero93@gmail.com>
2018-08-01 23:27:25 -0400

    removed redux boilerplate

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import configureStore from './store'
 import './index.css'
 import App from './Containers/App'
 
-ReactDOM.render(
-  <Provider store={configureStore()}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
